3a89849e229fe37799d1e2b671f48907e71e2236
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-07-30 13:34:21 -0300

    Service para criação de usuários

---
+++ api/src/services/CreateUserService.ts
@@ -0,0 +1,48 @@
+import { getCustomRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+import UsersRepository from '../repositories/UsersRepository';
+
+import AppError from '../error/AppError';
+
+interface RequestDTO {
+  name: string;
+  email: string;
+  password: string;
+  whatsapp?: string;
+}
+
+class CreateUserService {
+  public async execute({
+    name,
+    email,
+    password,
+    whatsapp,
+  }: RequestDTO): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    if (await usersRepository.findByEmail(email)) {
+      throw new AppError('Email address already used.');
+    }
+
+    if (whatsapp && (await usersRepository.findByWhatsapp(whatsapp))) {
+      throw new AppError('Whatsapp number already used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+      whatsapp,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
