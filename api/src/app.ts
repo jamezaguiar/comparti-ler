0bb9ab65ebf51f81374834087b18477afbeb89d8
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-07-30 13:40:55 -0300

    Global Exception Handler adicionado

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,15 +1,37 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import http from 'http';
 import socketio from 'socket.io';
 
+import AppError from './error/AppError';
+
 import routes from './routes';
+
 import './database';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 const server = http.createServer(app);
 const io = socketio(server);
