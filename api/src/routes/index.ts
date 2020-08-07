e0c5d821321bd0590245b534c94f68fca48b52d8
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-06 21:27:05 -0300

    Rotas wish

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import sessionsRouter from './sessions.routes';
 import usersRouter from './users.routes';
 import booksRouter from './books.routes';
+import wishesRouter from './wishes.routes';
 
 const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
 routes.use('/books', booksRouter);
+routes.use('/wishes', wishesRouter);
 
 export default routes;
