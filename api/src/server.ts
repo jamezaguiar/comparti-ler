5299ea368f7bfe5a878ea4c4bb993e36bab908e3
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-19 22:27:29 -0300

    Envio de possíveis empréstimos ao usuário via websocket

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,36 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
 import { Socket } from 'socket.io';
 import { server, io } from './app';
+import CheckPossibleLoansService from './services/CheckPossibleLoansService';
 
 const PORT = process.env.PORT || 3333;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-io.on('connection', (socket: Socket) => {
+interface ConnectedUsers {
+  [socket_id: string]: string;
+}
+
+const connectedUsers: ConnectedUsers = {};
+
+io.on('connection', async (socket: Socket) => {
   const { name, id } = socket.handshake.query;
   console.log(`Usuário ${name} de ID ${id} conectado.`);
+
+  connectedUsers[socket.id] = id;
+
+  const checkPossibleLoansService = new CheckPossibleLoansService();
+
+  const possibleLoans = await checkPossibleLoansService.execute(id);
+
+  for (const user in connectedUsers) {
+    io.to(user).emit('check_possible_loans', possibleLoans);
+    console.log(user);
+  }
+
+  socket.on('disconnect', () => {
+    console.log(`Usuário ${name} de ID ${id} desconectado.`);
+    delete connectedUsers[id];
+  });
 });
