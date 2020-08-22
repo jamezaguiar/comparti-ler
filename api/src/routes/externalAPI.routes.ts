63a7ffd24b0906b470c930f78b915aae1047c665
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-22 14:47:31 -0300

    Rota intermédiaria entre API e API externa para busca por ISBN

--- api/src/routes/externalAPI.routes.ts
+++ api/src/routes/externalAPI.routes.ts
@@ -1,8 +1,23 @@
 import { Router } from 'express';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import SearchBookByISBNService from '../services/SearchBookByISBNService';
 import SearchBooksByTitleService from '../services/SearchBooksByTitleService';
 
 const externalAPIRouter = Router();
+
+externalAPIRouter.use(ensureAuthenticated);
+
+externalAPIRouter.get('/searchBookByISBN', async (request, response) => {
+  const { isbn } = request.query as { isbn: string };
+
+  const searchBookByISBN = new SearchBookByISBNService();
+
+  const book = await searchBookByISBN.execute(isbn);
+
+  return response.json(book);
+});
 
 externalAPIRouter.get('/searchBooksByTitle', async (request, response) => {
   const { title } = request.query as { title: string };
---
+++ api/src/services/SearchBookByISBNService.ts
@@ -0,0 +1,42 @@
+import { booksAPI } from '../helpers/api';
+
+interface BookStatusDTO {
+  success: boolean;
+  code: number;
+  message?: string;
+}
+
+interface BookDataDTO {
+  titulo: string;
+  contribuicao: [
+    {
+      nome: string;
+      sobrenome: string;
+      tipo_de_contribuicao: string;
+      codigo_contribuicao: string;
+    },
+  ];
+  sinopse: string;
+  imagens: {
+    imagem_primeira_capa: {
+      pequena: string;
+      media: string;
+      grande: string;
+    };
+  };
+}
+
+interface APIResponseDTO {
+  books: BookDataDTO[];
+  status: BookStatusDTO;
+}
+
+class SearchBookByISBNService {
+  public async execute(isbn: string): Promise<APIResponseDTO> {
+    const response = await booksAPI.get<APIResponseDTO>(`?isbn=${isbn}`);
+
+    return response.data;
+  }
+}
+
+export default SearchBookByISBNService;
