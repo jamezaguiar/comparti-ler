3c8927e748bea2e51495ffdec8c86ce203a95f27
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-25 23:46:19 -0300

    Mudando campo borrowed para true ao aceitar empréstimo

--- api/src/services/AcceptRequestedLoanService.ts
+++ api/src/services/AcceptRequestedLoanService.ts
@@ -1,8 +1,10 @@
-import { getRepository } from 'typeorm';
+import { getRepository, getCustomRepository } from 'typeorm';
 
 import AppError from '../error/AppError';
 
 import Loan from '../models/Loan';
+
+import BooksRepository from '../repositories/BooksRepository';
 
 interface RequestDTO {
   loan_id: string;
@@ -10,6 +12,7 @@ interface RequestDTO {
 
 class AcceptRequestedLoanService {
   public async execute({ loan_id }: RequestDTO): Promise<Loan> {
+    const booksRepository = getCustomRepository(BooksRepository);
     const loansRepository = getRepository(Loan);
 
     const loan = await loansRepository.findOne({
@@ -21,9 +24,19 @@ class AcceptRequestedLoanService {
       throw new AppError('Loan not found.');
     }
 
+    const book = await booksRepository.findOne({
+      where: { id: loan.book_id, borrowed: false },
+    });
+
+    if (!book) {
+      throw new AppError('Book not found.');
+    }
+
     loan.status = 'accepted';
+    book.borrowed = true;
 
     await loansRepository.save(loan);
+    await booksRepository.save(book);
 
     return loan;
   }
