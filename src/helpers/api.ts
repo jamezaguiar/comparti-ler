ad2afb71eaddf25695b4d9b0229db98dd294028e
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-08-28 15:13:47 -0300

    API de testes alterada para produção

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const booksAPI = axios.create({
-  baseURL: 'https://sandbox.mercadoeditorial.org/api/v1.2/book',
+  baseURL: 'https://api.mercadoeditorial.org/api/v1.2/book',
 });
